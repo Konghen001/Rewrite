569dd278c5c232a703e2dc0b9f05f19b054cbcb6
Konghen001 <142234408+Konghen001@users.noreply.github.com>
2024-03-25 10:09:23 +0800

    Update bdmap.js

--- bdmap.js
+++ bdmap.js
@@ -19,8 +19,8 @@ hostname = newclient.map.baidu.com, yongche.baidu.com, ugc.map.baidu.com
 # 打车页
 //^https:\/\/yongche\.baidu\.com\/goorder\/passenger\/notice url reject-dict
 //^https:\/\/yongche\.baidu\.com\/gomarketing\/api\/activity\/talos\/activitycard\? url reject-dict
-//^https:\/\/yongche\.baidu\.com\/gomarketing\/api\/popup\/getentrancecordovaurl url reject-dict
-//^https:\/\/yongche\.baidu\.com\/goorder\/passenger\/baseinfo url reject-dict
+^https:\/\/yongche\.baidu\.com\/gomarketing\/api\/popup\/getentrancecordovaurl url reject-dict
+^https:\/\/yongche\.baidu\.com\/goorder\/passenger\/baseinfo url reject-dict
 
 # > 百度地图_首页底部推荐
 ^https?:\/\/ugc\.map\.baidu\.com\/govui\/rich_content url reject-200
